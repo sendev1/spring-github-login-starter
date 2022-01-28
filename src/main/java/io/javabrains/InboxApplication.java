b92c768683c90076a75778c27e138221493a6eb2
Senthil <senthilrajanr@gmail.com>
2022-01-27 23:02:16 -0600

    connect to cassendra and githun oauath

--- src/main/java/io/javabrains/InboxApplication.java
+++ src/main/java/io/javabrains/InboxApplication.java
@@ -6,13 +6,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 
 @SpringBootApplication
 @RestController
-public class SpringGitHubLoginApplication {
+public class InboxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGitHubLoginApplication.class, args);
+		SpringApplication.run(InboxApplication.class, args);
 	}
 
 	@RequestMapping("/user")
@@ -21,5 +22,10 @@ public class SpringGitHubLoginApplication {
 		return principal.getAttribute("name");
 	}
 	
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
 
 }
